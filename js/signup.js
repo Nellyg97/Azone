d70231ca61c3105a52c6fe9152df6a056a02c4b7
Nelly <nellygutzaltska6@gmail.com>
2020-08-04 21:56:55 +0300

    Testing crappy JS

--- js/signup.js
+++ js/signup.js
@@ -1,29 +1,64 @@
 function validatePass() {
-    var x = document.getElementById("input");
-    if (x.type === "password") {
-        x.type = "text";
+    var input = document.getElementById("input");
+    if (input.type === "password") {
+        input.type = "text";
     } else {
-        x.type = "password";
+        input.type = "password";
     }
 }
 
 function checkBox() {
-    const btn=document.getElementById("submit");
-    const box=document.getElementById("terms");
-    if (box.checked===true) {
-        btn.disabled=false;
-    } else  {
-        btn.disabled=true;
+    const btn = document.getElementById("submit");
+    const box = document.getElementById("terms");
+    if (box.checked === true) {
+        btn.disabled = false;
+    } else {
+        btn.disabled = true;
     }
 }
 
 function validateEmail() {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const emailBox=document.getElementById("email");
+    const emailBox = document.getElementById("email");
     if (re.test(emailBox.value)) {
         return true;
     } else {
         alert("You have entered an invalid email address!");
         return false;
+    }
+}
+
+
+function validatePsw {
+    const password = document.getElementById("input")
+    const form = document.getElementById("form")
+
+        let messages = []
+        if (password.value.length <= 6) {
+            messages.push("Password must be longer than 6 characters.")
+        }
+
+        if (password.value.length >= 20) {
+            messages.push("Password must be less than 20 characters")
+        }
+
+        if (password.value === "password") {
+            messages.push("Password can't be password")
+        }
+
+        if (messages.length > 0) {
+            e.preventDefault()
+        }
+    }
+}
+
+function validateRepeat {
+    let pass=document.getElementById("input")
+    let repeat=document.getElementById("psw-repeat")
+    let messages=[]
+    if (pass===repeat) {
+        messages.push("")
+    } else {
+        messages.push("Your passwords don't match")
     }
 }
